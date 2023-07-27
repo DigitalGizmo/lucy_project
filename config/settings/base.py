41aa62f57524285a5e817695ef15825cad68cf8c
Don Button <37458717+DigitalGizmo@users.noreply.github.com>
2023-07-27 13:00:35 -0400

    add static dirs

--- config/settings/base.py
+++ config/settings/base.py
@@ -125,7 +125,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
+STATICFILES_DIRS = [BASE_DIR / 'apps' / 'local_static']
+
 STATIC_URL = 'static/'
+
+STATIC_ROOT = BASE_DIR.parents[0] / "lucy-static"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
