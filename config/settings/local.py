e972003a4b8cd9bbda3512a23a5c95e46633bc28
Don Button <37458717+DigitalGizmo@users.noreply.github.com>
2023-07-28 14:23:50 -0400

    change trailing slash on asset path

--- config/settings/local.py
+++ config/settings/local.py
@@ -5,4 +5,4 @@ DEBUG = True
 
 # Custom setting
 # ASSET_PATH = BASE_DIR.parents[0] / "lucy-media"
-ASSET_PATH = "http://lucy-proto.deerfield-ma.org/assets/"
+ASSET_PATH = "http://lucy-proto.deerfield-ma.org/assets"
