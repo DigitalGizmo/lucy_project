b285ae102067fe70d66aef961abe822cc012cb40
Don Button <37458717+DigitalGizmo@users.noreply.github.com>
2023-08-03 10:32:26 -0400

    rename moment model to proof

--- apps/moments/urls.py
+++ apps/moments/urls.py
@@ -6,5 +6,5 @@ app_name="moments"
 
 urlpatterns = [
     # path("", views.PersonListView.as_view(), name="menu"),
-    path("moment-model", views.MomentTemplatelView.as_view(), name="moment-model"),
+    path("proof", views.MomentTemplatelView.as_view(), name="proof"),
 ]
--- apps/moments/views.py
+++ apps/moments/views.py
@@ -3,4 +3,4 @@ from django.views.generic import TemplateView, ListView, DetailView
 # from .models import 
 
 class MomentTemplatelView(TemplateView):
-    template_name = "moments/moment-model.html"
+    template_name = "moments/proof.html"
