f329cfae2c66c5e32eb6c120f053fca35c84f516
Don Button <37458717+DigitalGizmo@users.noreply.github.com>
2023-08-28 18:41:47 -0400

    wireframe with nav to moment

--- apps/moments/urls.py
+++ apps/moments/urls.py
@@ -5,6 +5,7 @@ from . import views
 app_name="moments"
 
 urlpatterns = [
-    # path("", views.PersonListView.as_view(), name="menu"),
+    path("", views.MomentListView.as_view(), name="menu"),
+    path("community-title", views.MomentTitlelView.as_view(), name="community_title"),
     path("proof", views.MomentTemplatelView.as_view(), name="proof"),
 ]
