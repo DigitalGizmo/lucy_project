feced491f3c91def55a2ef89ce4acf83d1e44285
Don Button <37458717+DigitalGizmo@users.noreply.github.com>
2023-08-29 14:47:05 -0400

    finish evidence with view

--- apps/evidence/urls.py
+++ apps/evidence/urls.py
@@ -5,7 +5,7 @@ from . import views
 app_name="evidence"
 
 urlpatterns = [
-    path("", views.MomentListView.as_view(), name="menu"),
-    path("community-title", views.MomentTitlelView.as_view(), name="community_title"),
-    path("proof", views.MomentTemplatelView.as_view(), name="proof"),
+    path("", views.EvidenceListView.as_view(), name="menu"),
+    # path("community-title", views.MomentTitlelView.as_view(), name="community_title"),
+    # path("proof", views.MomentTemplatelView.as_view(), name="proof"),
 ]
--- apps/evidence/views.py
+++ apps/evidence/views.py
@@ -2,13 +2,13 @@ from django.shortcuts import render
 from django.views.generic import TemplateView, ListView, DetailView
 # from .models import 
 
-class MomentListView(TemplateView):
+class EvidenceListView(TemplateView):
     # model = Moment
     # context_object_name = 'object_list'
-    template_name = 'moments/moment_list.html'
+    template_name = 'evidence/evidence_list.html'
 
-class MomentTitlelView(TemplateView):
-    template_name = "moments/moment_title.html"
+# class MomentTitlelView(TemplateView):
+#     template_name = "moments/moment_title.html"
 
-class MomentTemplatelView(TemplateView):
-    template_name = "moments/proof.html"
+# class MomentTemplatelView(TemplateView):
+#     template_name = "moments/proof.html"
