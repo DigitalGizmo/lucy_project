5f0e8fdef1352048ef0bb4894f153eb3fbf8df8a
Don Button <37458717+DigitalGizmo@users.noreply.github.com>
2023-10-13 10:34:38 -0400

    loading json from file

--- apps/moments/views.py
+++ apps/moments/views.py
@@ -21,42 +21,11 @@ class MomentModelTemplatelView(TemplateView):
 
 def FrameJsonView(request):
     CURR_DIR = Path(__file__).resolve().parent
-    print(f"path:{CURR_DIR / 'frames.json'}" )
-    # with open(CURR_DIR / 'frames.json') as f:
-    #     data = json.loads(f.read())
-    data = {
-        "cars": ["ford", "BMW", "Fiat"],
-        "community":          
-        [
-            {
-                "imageName": "02-dawn-house-color",
-                "storyText": "To be used later, in a loop",
-                "moreWhoLinks": [
-                    {"title": "Jenny Cole", 
-                        "type": "internal", "url": "/people"},
-                    {"title": "Cesar", 
-                        "type": "external", "url": "https://digitalgizmo.com/"}
-                ],
-                "moreTopicLinks": [
-                    {"title": "Sleeping Arrangements", 
-                        "type": "external", "url": "https://digitalgizmo.com/"},            
-                ]
-            },
-            {
-                "imageName": "03-candle-color",
-                "storyText": "Frame index 1 -- To be used later, in a loop",
-                "moreWhoLinks": [
-                    {"title": "Phillis Wheatley", 
-                        "type": "internal", "url": "/people"},
-                    {"title": "Cesar", 
-                        "type": "external", "url": "https://digitalgizmo.com/"}
-                ],
-                "moreTopicLinks": [
-                    {"title": "Women's work", 
-                        "type": "external", "url": "https://digitalgizmo.com/"}            
-                ]
-            }
-        ]
-    }
+    # print(f"path:{CURR_DIR / 'frames.json'}" )
+
+    with open(CURR_DIR / 'frames.json', encoding='utf-8') as data_file:
+        data = json.loads(data_file.read())
+
+    # print(data)
 
     return JsonResponse(data, safe=False)
