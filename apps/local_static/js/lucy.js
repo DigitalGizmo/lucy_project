8c5c950520a350850ffcb2a33a65d7b8c58232ee
Don Button <37458717+DigitalGizmo@users.noreply.github.com>
2023-10-20 11:16:10 -0400

    switch slim to modal

--- apps/local_static/js/lucy.js
+++ apps/local_static/js/lucy.js
@@ -1,8 +1,8 @@
 htmx.on("htmx:afterSwap", (e) => {
     console.log("inafterSwap, target id: " + e.detail.target.id)
-    if (e.detail.target.id == "slimpop-container") {
-        document.getElementById("slimpop-overlay").classList.remove("hidden");
-        // document.getElementById("slimpop-overlay").classList.add("unhidden");
+    if (e.detail.target.id == "modal-container") {
+        document.getElementById("modal-overlay").classList.remove("hidden");
+        // document.getElementById("modal-overlay").classList.add("unhidden");
 
     }
 })
@@ -10,6 +10,6 @@ htmx.on("htmx:afterSwap", (e) => {
 htmx.on("htmx:beforeSwap", (e) => {
     console.log("got to close")
     if (e.detail.target.id == "close-button") {
-        document.getElementById("slimpop-overlay").classList.add("hidden");
+        document.getElementById("modal-overlay").classList.add("hidden");
     }
 })
