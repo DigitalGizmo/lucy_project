d1c7e28490d58003ecd3f44678892410ab2f1d67
Don Button <37458717+DigitalGizmo@users.noreply.github.com>
2023-12-01 15:58:14 -0500

    expand person fields

--- apps/people/models.py
+++ apps/people/models.py
@@ -3,8 +3,36 @@ from django.db import models
 # Create your models here.
 
 class Person(models.Model):
-    first_name = models.CharField(max_length=32, blank=True, default='')
+    GENDER = (
+        ('select','please select'),
+        ('male','male'),
+        ('female','female'),
+        ('unknown', 'unknown'),
+    )
+    ENSLAVEMENT_STATUS = (
+        (1,'enslaved'),
+        (2,'enslaved, then free'),
+        (3,'always free'),
+        (4,'enslaver'),
+        (5,'not enslaver')
+    )
+    PROD_STATUS = (
+        (0,'entered'),
+        (1,'draft'),
+        (3,'show')
+    )
+    first_name = models.CharField(max_length=32)
     last_name = models.CharField(max_length=32, blank=True, default='')
+    birth_year = models.IntegerField(blank=True, null=True)
+    birth_month = models.IntegerField(blank=True, null=True)
+    birth_day = models.IntegerField(blank=True, null=True)
+    death_year = models.IntegerField(blank=True, null=True)
+    death_month = models.IntegerField(blank=True, null=True)
+    death_day = models.IntegerField(blank=True, null=True)
+    gender = models.CharField(default='select', max_length=12, choices=GENDER)
+    menu_blurb = models.TextField(blank=True, default='')
+    enslavement_status = models.IntegerField(default=0, choices=ENSLAVEMENT_STATUS)
+    prod_status = models.IntegerField(default=0, choices=PROD_STATUS)
 
     def __str__(self):
         return self.first_name + " " + self.last_name
