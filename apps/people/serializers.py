651b97b21d17701e0fedb0ffe25f7ce7bf238e72
Don Button <37458717+DigitalGizmo@users.noreply.github.com>
2023-12-01 17:07:47 -0500

    add person fields to serializer

--- apps/people/serializers.py
+++ apps/people/serializers.py
@@ -4,4 +4,9 @@ from .models import Person
 class PersonSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Person
-        fields = ['first_name', 'last_name']
+        fields = [
+            'first_name', 'last_name', 'gender', 'enslavement_status',
+            'birth_year', 'birth_month', 'birth_day',
+            'death_year', 'death_month', 'death_day',
+            'menu_blurb', 'prod_status'            
+        ]
